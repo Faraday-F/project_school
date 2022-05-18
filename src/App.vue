76e013226d6b056dccf5f78a2a5d2acd7d1825c8
Faraday Fernandes <faradat.jr@gmail.com>
2022-05-17 23:34:34 -0300

    agora sim

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">   
+    <Alunos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Alunos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Alunos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap');
+body{
+  background-color: #eee;
+  font-family: 'Roboto:ital', cursive;
+  display: grid;
+  justify-items: center;
 }
+
+body, html {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  
+}
+
+.btn{
+  background-color: #ca3e3e;
+  border: 1px solid black;
+  padding: 10px 20px;
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  border-radius: 5px;
+  border-bottom: 3px solid black; 
+}
+
+.btn:hover{
+  text-shadow: 1px 1px 1px black;
+  margin-top: 3px;
+  border-bottom: 1px solid black;
+
+}
+
+.btn_add{
+  background-color: #42ca3e;
+  border: 1px solid black;
+  padding: 10px 20px;
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  border-radius: 5px;
+  border-bottom: 3px solid black; 
+}
+
+.btn_add:hover{
+  text-shadow: 1px 1px 1px black;
+  margin-top: 3px;
+  border-bottom: 1px solid black;
+
+}
+
+.input{
+  border: 2px solid black;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+
+table {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+}
+
+table tr td{
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+table tr td:hover {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #6ec6f5;
+  margin-bottom: 2px;
+  color: #3e5252;
+
+}
+
+table thead th {
+  background-color: rgb(89, 156, 243) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center !important;
+
+}
+
+
+.colPequeno {
+  width: 5%;
+}
+
+
+
 </style>
---
+++ src/components/Alunos.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <div>
+
+      <input class="input" type="text" placeholder="Nome do aluno" v-model="nome"
+    v-on:keyup.enter="addaluno()"><button class="btn_add" @click="addaluno">
+      ADD
+    </button>
+
+    </div>
+    
+    
+
+    <table border="2px">
+      <thead>
+        <th>Matricula</th>
+        <th>Nome</th>
+        <th>Opçoes</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{index+1}}</td>
+          <!--td>{{aluno.id}}</!--td -->
+          <td>{{aluno.nome}}</td>
+          <td>
+            <button class="btn" @click="remover(aluno)">Remover
+            </button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum aluno encontrado.
+      </tfoot>
+    </table>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return{
+      titulo: 'Alunos.com.br',
+      nome: '',
+      alunos: [
+      {id: 0, nome: 'Roberta'},
+      {id: 1, nome: 'Luiz'},
+      {id: 2, nome: 'Giovana'}
+      ]
+    }
+  },
+  methods: {
+    addaluno() {
+        let _aluno = {
+        nome: this.nome
+        }
+        this.alunos.push(_aluno);
+        this.nome = '';
+      
+    },
+    remover(aluno) {
+      let indice = this.alunos.indexOf(aluno);
+      this.alunos.splice(indice, 1)
+
+    },
+  },
+  
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
